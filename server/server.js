0c3804b2a24a5dc2e9c06e0e869462668d32ae35
Amirthisvaran R P <94542833+Amirthisvaran@users.noreply.github.com>
2026-02-06 23:04:56 +0530

    Update import paths and refactor server.js

--- server/server.js
+++ server/server.js
@@ -1,31 +1,34 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
-import connectDB from './config/db.js'
-import { clerkWebHooks } from './controllers/webhooks.js'
-import companyRouter from './routes/companyRoute.js'
-import connectCloudinary from './config/cloudinary.js'
-import jobRouter from './routes/jobRoute.js'
-import userRouter from './routes/userRoute.js'
+import connectDB from '../config/db.js'
+import { clerkWebHooks } from '../controllers/webhooks.js'
+import companyRouter from '../routes/companyRoute.js'
+import connectCloudinary from '../config/cloudinary.js'
+import jobRouter from '../routes/jobRoute.js'
+import userRouter from '../routes/userRoute.js'
 import { clerkMiddleware } from '@clerk/express'
 
-// App config
 const app = express()
 
-// DB Connection
+// Connect services (safe for serverless)
 await connectDB()
 await connectCloudinary()
 
 // Middlewares
-app.use(cors());
+app.use(cors())
 app.use(express.json())
 app.use(clerkMiddleware())
 
 // Routes
 app.get('/', (req, res) => {
-    res.send('API Working!')
+  res.send('API Working!')
 })
+
 app.post('/webhooks', clerkWebHooks)
 app.use('/api/company', companyRouter)
 app.use('/api/jobs', jobRouter)
 app.use('/api/users', userRouter)
+
+// Export for Vercel
+export default app
